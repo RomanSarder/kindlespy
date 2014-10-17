acc3823aeb39a3f5b591d9f6ad62b57c4604d06a
Andrey Klochkov <andrey3diq@gmail.com>
2014-10-17 20:41:13 +0300

    Revert "#34 KindleSpy v3 Improvements"
    This reverts commit 58da37cc815300c1c554db4622d8213d0c94476f.

--- src/BookPageParser.js
+++ src/BookPageParser.js
@@ -73,28 +73,10 @@ BookPageParser.prototype.GetReviews = function(responseText) {
     }
 };
 
-BookPageParser.prototype.GetPrice = function(responseText, callback) {
+BookPageParser.prototype.GetPrice = function(responseText) {
     var priceBlock = $(responseText).find('#priceBlock b.priceLarge');
-    if(priceBlock && priceBlock.text().trim() !== '') {
-        callback(priceBlock.text().trim());
-        return;
-    }
-
-    var kindleEditionUrl = $(responseText).find('.tmm_bookTitle a:contains(Kindle Edition)').attr('href');
-    $.get(kindleEditionUrl, function(response){
-        var kindleEditionPriceBlock = $(response).find('#priceBlock b.priceLarge');
-        if(kindleEditionPriceBlock) {
-            callback(kindleEditionPriceBlock.text().trim());
-            return;
-        }
-        callback('');
-    });
+    return priceBlock ? priceBlock.text().trim() : "";
 };
-
-BookPageParser.prototype.GetPriceIfNotSet = function(price, responseText, callback) {
-    if(price) callback(price);
-    this.GetPrice(responseText, callback);
-}
 
 BookPageParser.prototype.GetSalesRank = function(responseText) {
     if (typeof responseText === "undefined" || responseText === "") return '0';
@@ -174,40 +156,41 @@ BookPageParser.prototype.GetBookData = function(url, price, reviews, callback) {
         if (typeof entryTitle === "undefined" || entryTitle === "") entryTitle = _this.GetAuthorTitle(responseText);
         if (typeof entryTitle === "undefined") return;
         if (!reviews) reviews = _this.GetReviews(responseText);
-        _this.GetPriceIfNotSet(price, responseText, function(entryPrice) {
-            var entrySalesRank = _this.GetSalesRank(responseText);
-            var entryEstSale = _this.GetEstSale(entrySalesRank);
-            var realPrice = _this._siteParser.ParsePrice(entryPrice);
-            var entrySalesRecv = _this.GetSalesRecv(entryEstSale, realPrice);
-            var entryPrintLength = _this.GetPrintLength(responseText);
-            var entryAuthor = _this.GetAuthor(responseText);
-            var entryGoogleSearchUrl = _this.GetGoogleSearchUrlByTitleAndAuthor(entryTitle, entryAuthor);
-            var entryImageUrl = _this.GetImageUrl(responseText);
-            _this.GetGoogleImageSearchUrl(responseText, url, function (entryGoogleImageSearchUrl) {
-                _this.GetDateOfPublication(responseText, function (entryDateOfPublication) {
-                    if (typeof entryPrice === "undefined") entryPrice = "0";
-                    if (typeof entryEstSale === "undefined") entryPrice = "0";
-                    if (typeof entrySalesRecv == "undefined") entrySalesRecv = "0";
-                    if (typeof reviews === "undefined") reviews = "0";
-                    if (typeof entrySalesRank === "undefined" || entrySalesRank.length < 1) entrySalesRank = "1";
-                    if (typeof entryPrintLength === "undefined" || entryPrintLength == '' || entryPrintLength == "NaN") entryPrintLength = "n/a";
-                    if (typeof entryAuthor === "undefined" || entryAuthor.length < 1) entryAuthor = "n/a";
-                    if (typeof entryImageUrl === "undefined")entryImageUrl = '';
+        if (!price) price = _this.GetPrice(responseText);
 
-                    callback({
-                        title: entryTitle,
-                        price: _this._siteParser.FormatPrice(realPrice),
-                        estSale: entryEstSale,
-                        salesRecv: entrySalesRecv,
-                        reviews: reviews,
-                        salesRank: entrySalesRank,
-                        printLength: entryPrintLength,
-                        author: entryAuthor,
-                        dateOfPublication: entryDateOfPublication,
-                        googleSearchUrl: entryGoogleSearchUrl,
-                        googleImageSearchUrl: entryGoogleImageSearchUrl,
-                        imageUrl: entryImageUrl
-                    });
+        var entrySalesRank = _this.GetSalesRank(responseText);
+        var entryEstSale = _this.GetEstSale(entrySalesRank);
+        var entryPrice = price;
+        var realPrice = _this._siteParser.ParsePrice(price);
+        var entrySalesRecv = _this.GetSalesRecv(entryEstSale, realPrice);
+        var entryPrintLength = _this.GetPrintLength(responseText);
+        var entryAuthor = _this.GetAuthor(responseText);
+        var entryGoogleSearchUrl = _this.GetGoogleSearchUrlByTitleAndAuthor(entryTitle, entryAuthor);
+        var entryImageUrl = _this.GetImageUrl(responseText);
+        _this.GetGoogleImageSearchUrl(responseText, url, function (entryGoogleImageSearchUrl) {
+            _this.GetDateOfPublication(responseText, function (entryDateOfPublication) {
+                if (typeof entryPrice === "undefined") entryPrice = "0";
+                if (typeof entryEstSale === "undefined") entryPrice = "0";
+                if (typeof entrySalesRecv == "undefined") entrySalesRecv = "0";
+                if (typeof reviews === "undefined") reviews = "0";
+                if (typeof entrySalesRank === "undefined" || entrySalesRank.length < 1) entrySalesRank = "1";
+                if (typeof entryPrintLength === "undefined" || entryPrintLength == '' || entryPrintLength == "NaN") entryPrintLength = "n/a";
+                if (typeof entryAuthor === "undefined" || entryAuthor.length < 1) entryAuthor = "n/a";
+                if (typeof entryImageUrl === "undefined")entryImageUrl = '';
+
+                callback({
+                    title: entryTitle,
+                    price: _this._siteParser.FormatPrice(realPrice),
+                    estSale: entryEstSale,
+                    salesRecv: entrySalesRecv,
+                    reviews: reviews,
+                    salesRank: entrySalesRank,
+                    printLength: entryPrintLength,
+                    author: entryAuthor,
+                    dateOfPublication: entryDateOfPublication,
+                    googleSearchUrl: entryGoogleSearchUrl,
+                    googleImageSearchUrl: entryGoogleImageSearchUrl,
+                    imageUrl: entryImageUrl
                 });
             });
         });
