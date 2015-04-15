fd1bebdaf29bdfb32efbd5a80316195091201bd0
Andrey Klochkov <andrey3diq@gmail.com>
2015-04-15 19:20:22 +0300

    #72: Firefox Extension: refactor exportToCSV

--- src/tab/MainTab.js
+++ src/tab/MainTab.js
@@ -67,50 +67,8 @@ MainTab.prototype.exportToCsv = function(data){
             x[index + 1][10] = bookData[index].Url;
         }
     }
-
-    var csvContent = "\uFEFF";
-    x.forEach(function(infoArray, index){
-        if (index <= bookData.length)
-        {
-            var dataString = [];
-            for (var i = 0; i < infoArray.length; i++)
-            {
-                var quotesRequired = false;
-                if (infoArray[i].indexOf(",") >= 0)
-                    quotesRequired = true;
-                var escapeQuotes = false;
-                if (infoArray[i].indexOf("\"") >= 0)
-                    escapeQuotes = true;
-
-                var fieldValue = (escapeQuotes ? infoArray[i].replace("\"", "\"\"") : infoArray[i]);
-
-                if (fieldValue.indexOf("\r") >= 0 || fieldValue.indexOf("\n") >= 0)
-                {
-                    quotesRequired = true;
-                    fieldValue = fieldValue.replace("\r\n", "");
-                    fieldValue = fieldValue.replace("\r", "");
-                    fieldValue = fieldValue.replace("\n", "");
-                }
-
-                if(i == 10 && index > 0)
-                    fieldValue =  "=HYPERLINK(\"" + fieldValue + "\")";
-                dataString[i] = (quotesRequired || escapeQuotes ? "\"" : "") + fieldValue + (quotesRequired || escapeQuotes ? "\"" : "") + ((i < (infoArray.length-1)) ? "," : "\r\n");
-            }
-            for (var i = 0; i < dataString.length; i ++)
-                csvContent += dataString[i];
-        }
-    });
-
-    var blob = new Blob([csvContent], {type : 'text/csv', charset : 'utf-8', encoding:'utf-8'});
-    var url = URL.createObjectURL(blob);
-    var today = new Date();
-    var dd = today.getDate();
-    var mm = today.getMonth()+1;
-    var yyyy = today.getFullYear();
-    var link = document.createElement("a");
-    link.setAttribute("href", url);
-    link.setAttribute("download", "bs-"+Helper.getCategoryFromBookData(bookData)+"-" + mm + "-" + dd + "-" + yyyy + ".csv");
-    link.click();
+    var fileName = "bs-" + Helper.getCategoryFromBookData(bookData);
+    Export.exportData(x, bookData.length, fileName);
 };
 
 MainTab.prototype.insertData = function(pageNumber, books, siteParser){
