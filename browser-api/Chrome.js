46d5055a594f8b03e68fb36216dcd67426b19d2d
Andrey Klochkov <andrey3diq@gmail.com>
2015-04-17 23:50:41 +0300

    #72: Firefox Extension: undefined check fixed for firefox

--- browser-api/Chrome.js
+++ browser-api/Chrome.js
@@ -26,7 +26,7 @@ Api.addListener = function(messageListener){
 Api.sendMessageToActiveTab = function(message, callback){
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         chrome.tabs.sendMessage(tabs[0].id, message, function (response) {
-            if (callback !== undefined && typeof(callback) === 'function')
+            if (typeof callback !== 'undefined' && typeof(callback) === 'function')
                 return callback(response);
         });
     });
