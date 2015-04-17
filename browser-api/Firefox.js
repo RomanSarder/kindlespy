30cfce0df8f751238e4e472837ccd774a46482db
Andrey Klochkov <andrey3diq@gmail.com>
2015-04-17 23:15:23 +0300

    #72: Firefox Extension: firefox storage API

--- browser-api/Firefox.js
+++ browser-api/Firefox.js
@@ -32,36 +32,36 @@ Api.sendMessageToActiveTab = function(message, callback){
         callback(result);
     });
 };
-/* Following code should be in main.js
- var workers = [];
- function initEventScript(worker){
- array.add(workers, worker);
- worker.on('pageshow', function() { array.add(workers, this); });
- worker.on('pagehide', function() { array.remove(workers, this); });
- worker.on('detach', function() { array.remove(workers, this); });
- }
+// TODO: Following code should be in main.js
+//var workers = [];
+//function initEventScript(worker){
+//    array.add(workers, worker);
+//    worker.on('pageshow', function() { array.add(workers, this); });
+//    worker.on('pagehide', function() { array.remove(workers, this); });
+//    worker.on('detach', function() { array.remove(workers, this); });
+//}
+//
+//popup.port.on('request', function(messageObject){
+//    for (var i = 0; i < workers.length; i += 1) {
+//        if (workers[i].tab.index === tabs.activeTab.index) {
+//            workers[i].port.emit('request', messageObject);
+//            workers[i].port.once('response-' + messageObject.id, function(result){
+//                popup.port.emit('response-' + messageObject.id, result);
+//            });
+//        }
+//    }
+//});
 
- popup.port.on('request', function(messageObject){
- for (var i = 0; i < workers.length; i += 1) {
- if (workers[i].tab.index === tabs.activeTab.index) {
- workers[i].port.emit('request', messageObject);
- workers[i].port.once('response-' + messageObject.id, function(result){
- popup.port.emit('response-' + messageObject.id, result);
- });
- }
- }
- });
- */
 
 Api.openNewTab = function(url){
     addon.port.emit('open-tab', url);
     addon.port.emit('close');
 };
-/* Following code should be in main.js
- popup.port.on('open-tab', function(url){
- tabs.open(url);
- });
- */
+// TODO: Following code should be in main.js
+//popup.port.on('open-tab', function(url){
+//    tabs.open(url);
+//});
+
 
 Api.registerOnShowEvent = function(eventListener){
     addon.port.on('show', eventListener);
@@ -69,21 +69,69 @@ Api.registerOnShowEvent = function(eventListener){
 
 Api.storage = {
     set: function(data, callback){
-        var port = (addon !== undefined) ? addon.port : self.port;
-        console.log(port);
-        var result = {};
-        result[key] = 'test value';
-        callback(result);
+        var port = (typeof addon !== 'undefined') ? addon.port : self.port;
+        var messageId = Date.now();
+        addon.port.emit('storage-set', {id: messageId, data: data});
+        addon.port.once('storage-set-response-' + messageId, function(result){
+            callback(result);
+        });
     },
     get: function(key, callback){
-        var port = (addon !== undefined) ? addon.port : self.port;
+        var port = (typeof addon !== 'undefined') ? addon.port : self.port;
         var messageId = Date.now();
-        addon.port.emit('storage-get', {id: messageId, key: key});
-        addon.port.once('storage-get-response-' + messageId, function(result){
+        port.emit('storage-get', {id: messageId, key: key});
+        port.once('storage-get-response-' + messageId, function(result){
             callback(result);
         });
     },
     clear: function(){
-
+        var port = (typeof addon !== 'undefined') ? addon.port : self.port;
+        port.emit('storage-clear');
     }
 };
+
+// TODO: Following code should be in main.js
+//function storageGet(port, messageObject){
+//    var result = {};
+//    result[messageObject.key] = simpleStorage.storage[messageObject.key];
+//    port.emit('storage-get-response-' + messageObject.id, result);
+//}
+//
+//function storageSet(port, messageObject){
+//    for (var property in messageObject.data){
+//        if (messageObject.data.hasOwnProperty(property))
+//            simpleStorage.storage[property] = messageObject.data[property];
+//    }
+//
+//    var bytesInUse = simpleStorage.quotaUsage * 5242880; // 5Mb
+//    port.emit('storage-set-response-' + messageObject.id, bytesInUse);
+//}
+//
+//function storageClear(){
+//    simpleStorage.storage = {};
+//}
+//
+// TODO: In init worker function
+//var port = worker.port;
+//port.on('storage-get', function(messageObject){
+//    storageGet(port, messageObject);
+//});
+//port.on('storage-set', function(messageObject){
+//    storageSet(port, messageObject);
+//});
+//port.on('storage-clear', function(messageObject){
+//    storageClear(port, messageObject);
+//});
+//
+// TODO: In popup events section
+//popup.port.on('storage-set', function(messageObject){
+//    storageSet(popup.port, messageObject);
+//});
+//
+//popup.port.on('storage-get', function(messageObject){
+//    storageGet(popup.port, messageObject);
+//});
+//
+//popup.port.on('storage-clear', function(){
+//    storageClear();
+//});
